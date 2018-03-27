be71b6dc506a39de9c34d870accab6e2c2c81c9d
weifengan <weifengan@163.com>
2018-03-27 22:23:04 +0800

    Site updated: 2018-03-27 22:22:39

---
+++ 2018/03/25/unity/u3d_call_cplusplusdll/u3d_call_dll/MyCSharpLib/MyCSharpLib/MyLib1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCSharpLib
+{
+    public class MyLib1
+    {
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public int By(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
